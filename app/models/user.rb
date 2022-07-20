7bed372cfde0eaf353296e8ba13f9097bdc54be0
Moeez Mubashar <moeezmubashar2@gmail.com>
2022-07-20 16:17:16 +0000

    validate users form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    validates :first_name, presence: true, length: {minimum: 3,maximum: 50}
+    validates :last_name, presence: true, length: {minimum: 5,maximum: 50}
+    validates :password, presence: true, length: {minimum: 5,maximum: 50}
+    validates :email, presence: true, length: {minimum: 5,maximum: 50}
 end
